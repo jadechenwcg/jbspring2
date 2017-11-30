2aba85514911d14902068dfb773b32bba1b8d5aa
Jade Chen <jade.chen@na.whiteclarkegroup.com>
2017-11-30 15:44:21 -0500

    lec 14, bean lifecycle and callback

--- src/main/java/com/jade/javabrains/DrwaingApp.java
+++ src/main/java/com/jade/javabrains/DrwaingApp.java
@@ -1,14 +1,14 @@
 package com.jade.javabrains;
 
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class DrwaingApp {
 
 	public static void main(String[] args) {
-		ApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");
-		
-		Triangle t = (Triangle) ctx.getBean("triangle1");
+		AbstractApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");		
+		ctx.registerShutdownHook();
+		Triangle t = (Triangle) ctx.getBean("triangle");
 		t.draw();
 
 	}
--- src/main/java/com/jade/javabrains/Triangle.java
+++ src/main/java/com/jade/javabrains/Triangle.java
@@ -2,29 +2,61 @@ package com.jade.javabrains;
 
 import java.util.List;
 
-import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.BeanNameAware;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
 
-public class Triangle {
+public class Triangle implements InitializingBean, DisposableBean {
+//public class Triangle  {
 
-	private List<Point> points;
+	private Point pA;
+	private Point pB;
+	private Point pC;
 
-	public List<Point> getPoints() {
-		return points;
+	public Point getpA() {
+		return pA;
 	}
 
-	public void setPoints(List<Point> points) {
-		this.points = points;
+	public void setpA(Point pA) {
+		this.pA = pA;
+	}
+
+	public Point getpB() {
+		return pB;
+	}
+
+	public void setpB(Point pB) {
+		this.pB = pB;
+	}
+
+	public Point getpC() {
+		return pC;
+	}
+
+	public void setpC(Point pC) {
+		this.pC = pC;
 	}
 
 	public void draw() {
-		for (Point p : points) {
-			System.out.println("Traingle drawn [" + p.getX() + "," + p.getY()
-					+ "]");
-		}
+		System.out.println("Traingle drawn [" + getpA().getX() + ","
+				+ getpA().getY() + "]" + "[" + getpB().getX() + ","
+				+ getpB().getY() + "]" + "[" + getpC().getX() + ","
+				+ getpC().getY() + "]");
+	}
 
+	public void afterPropertiesSet() throws Exception {
+		System.out.println("InitializingBean init method called for Triangle");
+	}
+
+	public void destroy() throws Exception {
+		System.out.println("DisposableBean destroy method called for Triangle");
+	}
+	
+	public void myInit(){
+		System.out.println("my init method called for Triangle");
+	}
+	
+	public void cleanup(){
+		System.out.println("cleanup method called for Triangle");
 	}
 
 }
