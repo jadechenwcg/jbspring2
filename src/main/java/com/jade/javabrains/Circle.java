bef14f24984dd61c005e419eb4d66e366e0ff977
Jade Chen <jade.chen@na.whiteclarkegroup.com>
2017-12-01 09:18:46 -0500

    <context:annotation-config /> and Autowired annotation

--- src/main/java/com/jade/javabrains/Circle.java
+++ src/main/java/com/jade/javabrains/Circle.java
@@ -1,6 +1,7 @@
 package com.jade.javabrains;
 
-import org.springframework.beans.factory.annotation.Required;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 public class Circle implements Shape {
 	private Point center;
@@ -9,7 +10,9 @@ public class Circle implements Shape {
 		return center;
 	}
 
-	@Required
+//	@Required
+	@Autowired //first scan type, 2nd scan name
+	@Qualifier("circleRelated")
 	public void setCenter(Point center) {
 		this.center = center;
 	}
